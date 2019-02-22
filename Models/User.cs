782a46287b464bbd75680370f9f8db3f6b304a25
Graham Rogers <gmrogers04@gmail.com>
2019-02-22 17:09:30 -0600

    working db

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GasTracker.Models {
+    public class User {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
